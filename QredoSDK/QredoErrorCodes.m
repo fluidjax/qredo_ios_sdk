1dd1069f19b5ea9368158769c0b64d30462ce8c2
Dmitry Matyukhin <dmitry@qredo.com>
2014-11-26 16:21:42 +0000

    Move error codes from LinguaFranca repo

---
+++ QredoSDK/QredoErrorCodes.m
@@ -0,0 +1,11 @@
+//
+//  QredoErrorCodes.m
+//  QredoSDK
+//
+//  Created by David Hearn on 04/11/2014.
+//
+//
+
+#import "QredoErrorCodes.h"
+
+NSString *const QredoErrorDomain = @"com.qredo.error";
---
+++ QredoSDK/include/QredoErrorCodes.h
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2011-2014 Qredo Ltd.  Strictly confidential.  All rights reserved.
+ */
+
+#import <Foundation/Foundation.h>
+
+// Domain used in NSError
+extern NSString *const QredoErrorDomain;
+
+
+typedef NS_ENUM(NSInteger, QredoErrorCode) {
+    QredoErrorCodeUnknown = 1000,
+    QredoErrorCodeRemoteOperationFailure,
+    QredoErrorCodeAppNotAuthorized,
+
+    // Vault errors
+    QredoErrorCodeVaultUnknown = 2000,
+    QredoErrorCodeVaultItemNotFound,
+    QredoErrorCodeVaultItemHasBeenDeleted,
+
+    // Rendezvous errors
+    QredoErrorCodeRendezvousNotFound = 3001,
+    QredoErrorCodeRendezvousInvalidData,
+    QredoErrorCodeRendezvousAlreadyExists,
+    QredoErrorCodeRendezvousUnknownResponse,
+    QredoErrorCodeRendezvousWrongAuthenticationCode,
+
+    // Conversation errors
+    QredoErrorCodeConversationUnknown = 4000,
+    QredoErrorCodeConversationNotFound,
+    QredoErrorCodeConversatioinInvalidData,
+    QredoErrorCodeConversationWrongAuthenticationCode,
+};
+
+
+
