8e6e182f4850632f57b36907e8fa08129ae0fa26
Dmitry Matyukhin <dmitry@qredo.com>
2014-12-03 18:45:28 +0000

    Moved the sequence diagrams to https://github.com/Qredo/qredo_ios_sdk/wiki/Keychain-Transporter

--- QredoSDK/QredoKeychainTransporter.h
+++ QredoSDK/QredoKeychainTransporter.h
@@ -14,57 +14,13 @@
 @end
 
 
-/*
- In https://www.websequencediagrams.com
-
- App->+QredoClient: receiveKeychainWithCompletionHandler:
- QredoClient->+QredoKeychainReceiverQR: <<new>>
- QredoKeychainReceiverQR-->-QredoClient: receiverDelegate
- QredoClient-> QredoKeychainReceiver: initWithDelegate:senderDelegate
-
- QredoClient->-QredoKeychainReceiver: start
-
- QredoKeychainReceiver-->+QredoKeychainReceiverQR: qredoKeychainReceiverWillCreateRendezvous
-
- QredoKeychainReceiverQR->-UI: show spinner
-
- QredoKeychainReceiver->QredoClient: createRendezvousWithTag:randomTag
- QredoClient->QredoRendezvous: <<new>>
- QredoClient-->QredoKeychainReceiver: rendezvous
-
- QredoKeychainReceiver-->QredoKeychainReceiverQR: didCreateRendezvousWithTag:tag
-
- QredoKeychainReceiverQR->UI: show QR code
-
- QredoKeychainReceiver->QredoRendezvous: startListening
-
- opt
- QredoRendezvous->QredoKeychainReceiver: didReceiveReponse(conversation)
- QredoKeychainReceiver->QredoKeychainReceiver: deviceInfo
- QredoKeychainReceiver->QredoConversation: publishMessage(deviceInfo)
-
- QredoKeychainReceiver->+QredoKeychainReceiverQR: didEstablishConnectionWithFingerprint
-
- QredoKeychainReceiverQR-->-UI: hide QR, show fingerprint, "waiting for keychain"
-
- QredoKeychainReceiver->QredoConversation: startListening
- opt
- QredoConversation->QredoKeychainReceiver: didReceiveNewMessage(keychain)
- QredoKeychainReceiver->QredoKeychainReceiver: parseKeychain
- QredoKeychainReceiver->QredoConversation: publishMessage (confirm)
- QredoKeychainReceiver->QredoConversation: stopListening
- QredoKeychainReceiver->QredoClient: installKeychain
- QredoClient->App:completionHandler
- end
- end
-*/
+// see https://github.com/Qredo/qredo_ios_sdk/wiki/Keychain-Transporter
 @protocol QredoKeychainReceiverDelegate <NSObject>
 
 @required
 
 // `cancelHandler` should be kept by the receiver delegate and called if user presses "Cancel" button.
 // However, after getting calls `qredoKeychainReceiverDidReceiveKeychain:` or `qredoKeychainReceiver:didFailWithError:`, `cancelHandler` shall not be called
-
 - (void)qredoKeychainReceiverWillCreateRendezvous:(QredoKeychainReceiver *)receiver;
 
 - (void)qredoKeychainReceiver:(QredoKeychainReceiver *)receiver didCreateRendezvousWithTag:(NSString*)tag cancelHandler:(void(^)())cancelHandler;
@@ -77,63 +33,6 @@
 
 @end
 
-/*
- In https://www.websequencediagrams.com
-
- App->+QredoClient: sendKeychainWithCompletionHandler:
- QredoClient->+QredoKeychainSenderQR: <<new>>
- QredoKeychainSenderQR-->-QredoClient: senderDelegate
- QredoClient->QredoKeychainSender: initWithDelegate:senderDelegate
-
- QredoClient->-QredoKeychainSender: start
-
- QredoKeychainSender-->QredoKeychainSenderQR: qredoKeychainSenderDiscoveringRendezvous
-
- QredoKeychainSenderQR->UI: show QR scanner
- UI-->QredoKeychainSenderQR: scanned tag
-
-
- QredoKeychainSenderQR->+QredoKeychainSender: completionHander(tag)
- note right of QredoKeychainSender: verify tag (length, characters)
- QredoKeychainSender-->-QredoKeychainSenderQR: true
-
- QredoKeychainSenderQR-->UI: hide QR scanner
-
- QredoKeychainSender->QredoClient: respondWithTag:tag
-
- QredoClient->+QredoConversation: <<new>>
- QredoClient-->QredoKeychainSender: conversation
- QredoKeychainSender
-
- QredoKeychainSender->QredoConversation: startListening
-
- note over QredoKeychainSender, QredoConversation: Receiving device info
-
- QredoConversation->QredoKeychainSender: didReceiveNewMessage (device-name)
-
- QredoKeychainSender->+QredoKeychainSenderQR: didEstablishConnectionWithDevice
-
- QredoKeychainSenderQR->UI: show confirmation
- UI->QredoKeychainSenderQR: "confirm"
-
- QredoKeychainSenderQR->-QredoKeychainSender: confimrationHandler(true)
-
- note over QredoKeychainSender, QredoConversation: Sending keychain
-
- QredoKeychainSender->QredoClient: getKeychain (with device unlock)
- QredoClient->QredoKeychainSender: keychain
-
- QredoKeychainSender->QredoConversation: publishMessage(serializedKeychain)
-
- note over QredoKeychainSender, QredoConversation: Receive confirmation
-
- QredoConversation->QredoKeychainSender: didReceiveNewMessage (keychain-received)
-
- QredoKeychainSender->QredoKeychainSenderQR: didFinishSending
- QredoKeychainSenderQR->UI: hide
-
- QredoKeychainSender->App: completionHandler
- */
 @protocol QredoKeychainSenderDelegate <NSObject>
 
 @required
