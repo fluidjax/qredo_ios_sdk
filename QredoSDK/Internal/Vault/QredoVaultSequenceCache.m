e4c8620752adc6594e2bf3ac6b9a096a7bed7d08
Dmitry Matyukhin <dmitry@qredo.com>
2014-12-18 18:06:16 +0000

    Protect access to NSUserDefaults

--- QredoSDK/Internal/Vault/QredoVaultSequenceCache.m
+++ QredoSDK/Internal/Vault/QredoVaultSequenceCache.m
@@ -4,6 +4,8 @@
     NSMutableDictionary *_itemSequenceIds;
     NSMutableDictionary *_itemSequenceValues;
     QredoVaultSequenceValue *_sequenceValue;
+
+    dispatch_queue_t _dataQueue;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -12,6 +14,7 @@
 
 - (instancetype)init {
     self = [super init];
+    _dataQueue = dispatch_queue_create("com.qredo.vault.sequencecache", 0);
     _itemSequenceIds    = [self loadItemSequenceIds];
     _itemSequenceValues = [self loadItemSequenceValues];
     _sequenceValue      = [self loadSequenceValue];
@@ -46,8 +49,10 @@
 }
 
 - (void)saveSequenceValue:(NSNumber *)sequenceValue {
-    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-    [userDefaults setObject:_sequenceValue forKey:@"QredoVaultSequenceValue"];
+    dispatch_sync(_dataQueue, ^{
+        NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+        [userDefaults setObject:_sequenceValue forKey:@"QredoVaultSequenceValue"];
+    });
 }
 
 - (QredoVaultSequenceId *)sequenceIdForItem:(QredoVaultItemId *)itemId {
@@ -74,43 +79,59 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 - (NSNumber *)loadSequenceValue {
-    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-    NSNumber *maybeSequenceValue = [userDefaults objectForKey:@"QredoVaultSequenceValue"];
-    if (maybeSequenceValue == nil) {
-        return @1;
-    } else {
-        return maybeSequenceValue;
-    }
+    __block NSNumber *returnValue = nil;
+    dispatch_sync(_dataQueue, ^{
+        NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+        NSNumber *maybeSequenceValue = [userDefaults objectForKey:@"QredoVaultSequenceValue"];
+        if (maybeSequenceValue == nil) {
+            returnValue = @1;
+        } else {
+            returnValue = maybeSequenceValue;
+        }
+    });
+    return returnValue;
 }
 
 - (NSMutableDictionary *)loadItemSequenceIds {
-    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-    NSMutableDictionary *maybeItemSequenceIds = [userDefaults objectForKey:@"QredoVaultItemSequenceIds"];
-    if (maybeItemSequenceIds == nil) {
-        return [NSMutableDictionary new];
-    } else {
-        return [maybeItemSequenceIds mutableCopy];
-    }
+    __block NSMutableDictionary *result = nil;
+    dispatch_sync(_dataQueue, ^{
+        NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+        NSMutableDictionary *maybeItemSequenceIds = [userDefaults objectForKey:@"QredoVaultItemSequenceIds"];
+        if (maybeItemSequenceIds == nil) {
+            result = [NSMutableDictionary new];
+        } else {
+            result = [maybeItemSequenceIds mutableCopy];
+        }
+    });
+    return result;
 }
 
 - (NSMutableDictionary *)loadItemSequenceValues {
-    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-    NSMutableDictionary *maybeItemSequenceValues = [userDefaults objectForKey:@"QredoVaultItemSequenceValues"];
-    if (maybeItemSequenceValues == nil) {
-        return [NSMutableDictionary new];
-    } else {
-        return [maybeItemSequenceValues mutableCopy];
-    }
+    __block NSMutableDictionary *result = nil;
+    dispatch_sync(_dataQueue, ^{
+        NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+        NSMutableDictionary *maybeItemSequenceValues = [userDefaults objectForKey:@"QredoVaultItemSequenceValues"];
+        if (maybeItemSequenceValues == nil) {
+            result = [NSMutableDictionary new];
+        } else {
+            result = [maybeItemSequenceValues mutableCopy];
+        }
+    });
+    return result;
 }
 
 - (void)saveItemSequenceIds {
-    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-    [userDefaults setObject:_itemSequenceIds forKey:@"QredoVaultItemSequenceIds"];
+    dispatch_sync(_dataQueue, ^{
+        NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+        [userDefaults setObject:_itemSequenceIds forKey:@"QredoVaultItemSequenceIds"];
+    });
 }
 
 - (void)saveItemSequenceValues {
-    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
-    [userDefaults setObject:_itemSequenceValues forKey:@"QredoVaultItemSequenceValues"];
+    dispatch_sync(_dataQueue, ^{
+        NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+        [userDefaults setObject:_itemSequenceValues forKey:@"QredoVaultItemSequenceValues"];
+    });
 }
 
 - (NSNumber *)sequenceValue {
