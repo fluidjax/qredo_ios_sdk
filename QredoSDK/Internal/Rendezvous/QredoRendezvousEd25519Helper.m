f1074c877e1d80d6cbb774a0ff0aa5ad8685c237
Gabriel Radu <1342gr@gmail.com>
2015-01-11 22:51:10 +0000

    Implement rendezvous helper for ed25519

--- QredoSDK/Internal/Rendezvous/QredoRendezvousEd25519Helper.m
+++ QredoSDK/Internal/Rendezvous/QredoRendezvousEd25519Helper.m
@@ -5,8 +5,50 @@
 #import "QredoRendezvousEd25519Helper.h"
 #import "QredoRendezvousHelper_Private.h"
 #import "QredoClient.h"
+#import "CryptoImpl.h"
+#import "QredoBase58.h"
+
+
+@interface QredoRendezvousEd25519Helper () {
+    QredoED25519SigningKey *_sk;
+    QredoED25519VerifyKey *_vk;
+}
+@end
+
 
 @implementation QredoRendezvousEd25519Helper
+
+QredoRendezvousAuthSignature *kEmptySignature = nil;
+
++ (void)load
+{
+    [super load];
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        kEmptySignature = [QredoRendezvousAuthSignature rendezvousAuthED25519WithSignature:[NSMutableData dataWithLength:64]];
+    });
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+        if ([self noTagProvided]) {
+            
+            _sk = [self.cryptoImpl qredoED25519SigningKey];
+            _vk = _sk.verifyKey;
+            
+        } else {
+        
+            _sk = nil;
+            _vk = [self verifyKeyFromTag:self.tag];
+            
+        }
+        
+    }
+    return self;
+}
 
 - (QredoRendezvousAuthenticationType)type
 {
@@ -15,20 +57,30 @@
 
 - (NSString *)tag
 {
-    // TODO [GR]: Implement this.
-    return nil;
+    NSString *trimedString = [self.tag stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+
+    if ([trimedString length] == 0) {
+        return [QredoBase58 encodeData:_vk.data];
+    }
+    
+    if ([trimedString hasSuffix:@"@"]) {
+        NSString *encodedVK = [QredoBase58 encodeData:_vk.data];
+        return [trimedString stringByAppendingString:encodedVK];
+    }
+    
+    return self.tag;
 }
 
 - (QredoRendezvousAuthSignature *)emptySignature
 {
-    // TODO [GR]: Implement this.
-    return nil;
+    return kEmptySignature;
 }
 
 - (QredoRendezvousAuthSignature *)signatureWithData:(NSData *)data
 {
-    // TODO [GR]: Implement this.
-    return nil;
+    NSAssert(_sk, @"Signing key is unknown");    
+    NSData *sig = [self.cryptoImpl qredoED25519SignMessage:data withKey:_sk];
+    return [QredoRendezvousAuthSignature rendezvousAuthED25519WithSignature:sig];
 }
 
 - (BOOL)isValidSignature:(QredoRendezvousAuthSignature *)signature rendezvousData:(NSData *)rendezvousData
@@ -52,8 +104,34 @@
         return NO;
     }
     
-    // TODO [GR]: Implement this.
-    return NO;
+    return [self.cryptoImpl qredoED25519VerifySignature:signatureData ofMessage:rendezvousData verifyKey:_vk];
+}
+
+- (BOOL)noTagProvided
+{
+    NSString *trimedString = [self.tag stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    return [trimedString length] == 0 || [trimedString hasSuffix:@"@"];
+}
+
+- (QredoED25519VerifyKey *)verifyKeyFromTag:(NSString *)tag
+{
+    NSAssert([tag length], @"Malformed tag");
+    QredoED25519VerifyKey *vk = nil;
+    
+    NSUInteger prefixPos = [tag rangeOfString:@"@"].location;
+    
+    NSString *vkString = nil;
+    if (prefixPos == NSNotFound) {
+        vkString = tag;
+    } else {
+        vkString = [tag substringFromIndex:prefixPos+1];
+    }
+    
+    NSData *vkData = [QredoBase58 decodeData:tag];
+    NSAssert([tag length], @"Malformed tag (on decoding");
+    vk = [self.cryptoImpl qredoED25519VerifyKeyWithData:vkData];
+    
+    return vk;
 }
 
 @end
