24c28403adfb9e22fe84c1836bcadceefd3a42c5
Gabriel Radu <1342gr@gmail.com>
2015-01-16 11:37:12 +0000

    Add more tests for QredoBase58

--- QredoSDKTests/Base58Tests.m
+++ QredoSDKTests/Base58Tests.m
@@ -10,13 +10,43 @@
 
 
 
+static inline NSData *createRandomData() {
+    
+    const NSUInteger dataLength = rand() % 64;
+    unsigned char *dataBytes = malloc(dataLength);
+    
+    // Generate random data.
+    for (int byteIndex = 0; byteIndex < dataLength; byteIndex++) {
+        dataBytes[byteIndex] = rand() % 0x100;
+    }
+    
+    return [NSData dataWithBytesNoCopy:dataBytes length:dataLength];
+    
+}
+
+static inline NSString *createRandomEncodedValue() {
+    
+    static const char kAlphabet[] = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+    
+    const NSUInteger dataLength = rand() % 100;
+    unichar *dataBytes = malloc(dataLength * sizeof(const unichar));
+    
+    // Generate random data.
+    for (int byteIndex = 0; byteIndex < dataLength; byteIndex++) {
+        dataBytes[byteIndex] = kAlphabet[rand() % 58];
+    }
+    
+    return [NSString stringWithCharacters:dataBytes length:dataLength];
+}
+
+
 @interface Base58Tests : XCTestCase
 
 @end
 
 @implementation Base58Tests
 
-- (void)testSimpleEncoding {
+- (void)test_0010_SimpleEncoding {
     
     NSMutableData *data8 = [NSMutableData dataWithLength:1];
     unsigned char *data8Bytes = [data8 mutableBytes];
@@ -28,7 +58,7 @@
     
 }
 
-- (void)testSimpleDecoding {
+- (void)test_0020_SimpleDecoding {
     
     NSString *string9 = @"9";
     NSData *data8 = [QredoBase58 decodeData:string9];
@@ -42,7 +72,7 @@
     
 }
 
-- (void)testEncodingAndDecoding {
+- (void)test_0030_EncodingAndDecoding {
     
     NSData *dataToEncode = [@"My test data" dataUsingEncoding:NSUTF8StringEncoding];
     
@@ -55,8 +85,7 @@
     
 }
 
-- (void)testEncodingAndDecodingWithZerosPadding {
-    
+- (void)test_0040_EncodingAndDecodingWithZerosPadding {
     
     NSData *nonZeroPaddedData = [@"My test data" dataUsingEncoding:NSUTF8StringEncoding];
     NSMutableData *zeroPaddedData = [NSMutableData dataWithLength:5];
@@ -73,7 +102,115 @@
     
 }
 
+- (void)test_0050_EncodingAndDecodingRandomEquality {
+    
+    NSUInteger kIterations = 1000000;
+    srand ( (int)time(NULL) );
+    
+    for (int i = 1; i <= kIterations; i++) {
+        
+        NSData *dataToEncode = createRandomData();
+        NSString *encodedValue = [QredoBase58 encodeData:dataToEncode];
+        NSData *decodedData = [QredoBase58 decodeData:encodedValue];
+        
+        if (![dataToEncode isEqual:decodedData]) {
+            XCTFail("Encoding or decoding faild. Data to encode: <%@>, decoded data: <%@>",
+                    dataToEncode, decodedData);
+        }
+        
+        if ((i % 10000) == 0) {
+            NSLog(@"[Base58Tests testRandomEquality] iterations: %@k", @(i/1000));
+        }
+        
+    }
+    
+}
+
+- (void)test_0060_DecodingAndEncodingRandomEquality {
+    
+    NSUInteger kIterations = 1000000;
+    srand ( (int)time(NULL) );
+    
+    for (int i = 1; i <= kIterations; i++) {
+        
+        NSString *valueToDecode = createRandomEncodedValue();
+        NSData *encodedData = [QredoBase58 decodeData:valueToDecode];
+        NSString *encodedValue = [QredoBase58 encodeData:encodedData];
+        
+        if (![valueToDecode isEqualToString:encodedValue]) {
+            XCTFail("Encoding or decoding faild. Value to encode: <%@>, encoded value: <%@>",
+                    valueToDecode, encodedValue);
+        }
+        
+        if ((i % 10000) == 0) {
+            NSLog(@"[Base58Tests testRandomEquality] iterations: %@k", @(i/1000));
+        }
+        
+    }
+    
+}
+
+- (void)test_0070_EncodingRandomInequality {
+    
+    NSUInteger kIterations = 1000000;
+    srand ( (int)time(NULL) );
+    
+    for (int i = 1; i <= kIterations; i++) {
+        
+        
+        NSData *dataToEncode1 = createRandomData();
+        NSData *dataToEncode2 = createRandomData();
+        while ([dataToEncode1 isEqual:dataToEncode2]) {
+            dataToEncode2 = createRandomData();
+        }
+        
+        NSString *encodedValue1 = [QredoBase58 encodeData:dataToEncode1];
+        NSString *encodedValue2 = [QredoBase58 encodeData:dataToEncode2];
+        
+        if ([encodedValue1 isEqual:encodedValue2]) {
+            XCTFail("Encoding of diffent data resulted in same encoded value. Data to encode 1: <%@>, data to encode 2: <%@>, endoded value: <%@>",
+                    dataToEncode1, dataToEncode2, encodedValue1);
+        }
+        
+        if ((i % 10000) == 0) {
+            NSLog(@"[Base58Tests testRandomEquality] iterations: %@k", @(i/1000));
+        }
+        
+    }
+    
+}
+
+- (void)test_0080_DecodingRandomInequality {
+    
+    NSUInteger kIterations = 1000000;
+    srand ( (int)time(NULL) );
+    
+    for (int i = 1; i <= kIterations; i++) {
+        
+        NSString *valueToDecode1 = createRandomEncodedValue();
+        NSString *valueToDecode2 = createRandomEncodedValue();
+        while ([valueToDecode1 isEqualToString:valueToDecode2]) {
+            valueToDecode2 = createRandomEncodedValue();
+        }
+        
+        NSData *decodedData1 = [QredoBase58 decodeData:valueToDecode1];
+        NSData *decodedData2 = [QredoBase58 decodeData:valueToDecode2];
+        
+        if ([decodedData1 isEqual:decodedData2]) {
+            XCTFail("Decoding of diffent values resulted in same decoded data. Value to decode 1: <%@>, value to decode 2: <%@>, decoded data: <%@>",
+                    valueToDecode1, valueToDecode2, decodedData1);
+        }
+        
+        if ((i % 10000) == 0) {
+            NSLog(@"[Base58Tests testRandomEquality] iterations: %@k", @(i/1000));
+        }
+        
+    }
+    
+}
+
 @end
+
 
 
 
