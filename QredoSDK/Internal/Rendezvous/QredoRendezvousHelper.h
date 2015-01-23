4b0218965e8385a648b2a0f2c12ce5b02dd77ebe
Gabriel Radu <1342gr@gmail.com>
2015-01-23 20:22:02 +0000

    Fix some code comments

--- QredoSDK/Internal/Rendezvous/QredoRendezvousHelper.h
+++ QredoSDK/Internal/Rendezvous/QredoRendezvousHelper.h
@@ -62,7 +62,10 @@ typedef NS_ENUM(NSUInteger, QredoRendezvousHelperError) {
 /**
  * Gets the lingua franca signature type with the signature taken over a byte array.
  *
- * @param rendezvousData Data to be signed
+ * @param data  Data to be signed.
+ * @param error On input, a pointer to an error object. If an error occurs, this pointer is set to an actual 
+ *              error object containing the error information. You may specify nil for this parameter if you do 
+ *              not want the error information.
  * @return The signature type for the rendezvous with a signature.
  */
 - (QredoRendezvousAuthSignature *)signatureWithData:(NSData *)data error:(NSError **)error;
@@ -76,9 +79,12 @@ typedef NS_ENUM(NSUInteger, QredoRendezvousHelperError) {
 /**
  * Verify the signature
  *
- * @param rendezvousData Data to be signed (does not include the signature)
- * @param signatureData  The signature
- * @return True if the signature is valid, false otherwise
+ * @param rendezvousData    Data to be signed (does not include the signature).
+ * @param signature         The signature.
+ * @param error             On input, a pointer to an error object. If an error occurs, this pointer is set to an
+ *                          actual error object containing the error information. You may specify nil for this 
+ *                          parameter if you do not want the error information.
+ * @return True if the signature is valid, false otherwise.
  */
 - (BOOL)isValidSignature:(QredoRendezvousAuthSignature *)signature rendezvousData:(NSData *)rendezvousData error:(NSError **)error;
 
