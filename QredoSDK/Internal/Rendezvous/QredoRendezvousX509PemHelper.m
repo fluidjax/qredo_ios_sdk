9a6326d7a8cf9c6b1baa69a90ca93e5331875d0d
David Hearn <david.hearn@chyp.com>
2015-01-27 17:53:11 +0000

    Add QredoRendezvousX509PemHelper

---
+++ QredoSDK/Internal/Rendezvous/QredoRendezvousX509PemHelper.m
@@ -0,0 +1,9 @@
+/*
+ *  Copyright (c) 2011-2014 Qredo Ltd.  Strictly confidential.  All rights reserved.
+ */
+
+#import "QredoRendezvousX509PemHelper.h"
+
+@implementation QredoRendezvousX509PemHelper
+
+@end
