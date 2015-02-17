78dfe8b5d921498e0d79c3c06abb969e33f5b838
David Hearn <david.hearn@chyp.com>
2015-02-17 11:18:44 +0000

    Populated minimum RSA 2048bit public key length

--- QredoSDK/Internal/Rendezvous/QredoRendezvousRsa2048PemHelper.m
+++ QredoSDK/Internal/Rendezvous/QredoRendezvousRsa2048PemHelper.m
@@ -19,8 +19,25 @@
 const NSInteger kRsa2048AuthenticatedRendezvousSaltLength = 8;
 static const NSUInteger kRsa2048AuthenticatedRendezvousEmptySignatureLength = 256;
 
-// TODO: DH - confirm the minimum length of RSA 2048 authentication tag (i.e. single RSA 2048 bit Public key as PEM)
-static const NSUInteger kMinRsa2048AuthenticationTagLength = 1;
+/*
+ PEM RSA Public Key is a DER encoded public key with PEM wrapping.
+ Actual DER encoding lengths could vary with PKCS#1/X.509, exponent size. Smallest
+ would be PKCS#1 which is just 2 integers (2048 bit Modulus and minimum 1 byte Exponent) wrapped in a sequence,
+ along with tag/length bytes.  X.509 format would add extra wrapping (approx 20 bytes extra BEFORE base64, meaning
+ 28 bytes after base64).
+ 
+ PKCS#1 format calcs:
+ Modulus = 256 bytes data + 3 bytes length (82 + xxxx length) + 1 byte INTEGER tag = 260 bytes.
+ Exponent = 1 byte (min) data + 1 byte length + 1 byte INTEGER tag = 3 bytes.
+ Sequence wrapper = Modulus + exponent lengths of data + 3 bytes length + 1 byte tag = 263 + 3 + 1 = 267 bytes.
+ 
+ Base64 encodes 3 bytes as 4 bytes, 267 bytes = 356 bytes as base64 (ignoring any CR/CRLF line-wrapping breaks).
+ 
+ PEM Key header = 27 bytes (inc trailing CR). PEM Key footer = 26 bytes (inc leading and trailling CR)
+ 
+ Total min length of 2048 bit RSA key in PKCS#1 DER + PEM format = 409 bytes. 437 bytes for X.509 format.
+*/
+static const NSUInteger kMinRsa2048AuthenticationTagLength = 409;
 
 static const NSUInteger kRsa2048KeyLengthBits = 2048;
 static const NSUInteger kRandomKeyIdentifierLength = 32;
