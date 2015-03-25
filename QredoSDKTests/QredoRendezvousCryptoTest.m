ce1ed56ce08c3d15c27f3708a40d00cd3e9e5d35
Dmitry Matyukhin <dmitry@qredo.com>
2015-03-25 16:50:41 +0000

    Generator of test vectors

--- QredoSDKTests/QredoRendezvousCryptoTest.m
+++ QredoSDKTests/QredoRendezvousCryptoTest.m
@@ -41,4 +41,88 @@
     [self common_TestDerrivedKeysNotNilWithTag:@"any trusted tag@public key data"];
 }
 
+
+- (void)common_TestVectorsWithTag:(NSString *)tag {
+    QredoRendezvousCrypto *rendezvousCrypto = [QredoRendezvousCrypto instance];
+
+
+    NSLog(@"============ BEGIN ============");
+    NSData *masterKey = [rendezvousCrypto masterKeyWithTag:tag];
+    NSLog(@"Rendezvous tag: \"%@\"", tag);
+    NSLog(@"Master key: %@", masterKey);
+
+    QLFRendezvousHashedTag *hashedTag = [rendezvousCrypto hashedTagWithMasterKey:masterKey];
+    NSLog(@"Hashed tag: %@", [hashedTag data]);
+
+    NSData *authKey = [rendezvousCrypto authenticationKeyWithMasterKey:masterKey];
+    NSLog(@"Authentication key: %@", authKey);
+
+    NSData *encKey = [rendezvousCrypto encryptionKeyWithMasterKey:masterKey];
+    NSLog(@"Encryption key: %@", encKey);
+
+
+    QLFKeyPairLF *requesterKeyPair  = [rendezvousCrypto newRequesterKeyPair];
+    NSData *requesterPublicKeyBytes = [[requesterKeyPair pubKey] bytes];
+    NSString *conversationType      = @"com.qredo.chat";
+
+    NSLog(@"---");
+    NSLog(@"Responder Info:");
+    NSLog(@"Requester public key: %@", requesterPublicKeyBytes);
+    NSLog(@"Conversation type: \"%@\"", conversationType);
+
+
+
+
+    QLFRendezvousResponderInfo *responderInfo
+    = [QLFRendezvousResponderInfo rendezvousResponderInfoWithRequesterPublicKey:requesterPublicKeyBytes
+                                                               conversationType:conversationType
+                                                                       transCap:[NSSet set]];
+
+    NSData *marshalledResponderInfo = [QredoPrimitiveMarshallers marshalObject:responderInfo];
+    NSLog(@"Marshalled responder info %@", marshalledResponderInfo);
+    NSLog(@"---");
+
+    NSData *encryptedResponderInfo = [rendezvousCrypto encryptResponderInfo:responderInfo encryptionKey:encKey];
+    NSLog(@"Encrypted responder info %@", encryptedResponderInfo);
+
+    NSData *authenticationCode = [rendezvousCrypto authenticationCodeWithHashedTag:hashedTag
+                                                                 authenticationKey:authKey
+                                                            encryptedResponderData:encryptedResponderInfo];
+
+    NSLog(@"Authentication code: %@", authenticationCode);
+    NSLog(@"============ END ============");
+}
+
+- (void)testGenerateTestVectors
+{
+    [self common_TestVectorsWithTag:@"simple tag"];
+    [self common_TestVectorsWithTag:@"fcc989f23ff77dd956cc5cde637c2d7eb07376dcf7322565e265e7f0913b5ad9"];
+
+    QredoRendezvousCrypto *rendezvousCrypto = [QredoRendezvousCrypto instance];
+    id<QredoRendezvousCreateHelper> rendezvousHelper
+    = [rendezvousCrypto rendezvousHelperForAuthenticationType:QredoRendezvousAuthenticationTypeEd25519
+                                                      fullTag:@"Ed25519@"
+                                               signingHandler:nil
+                                                        error:nil];
+
+    [self common_TestVectorsWithTag:rendezvousHelper.tag];
+
+    rendezvousHelper
+    = [rendezvousCrypto rendezvousHelperForAuthenticationType:QredoRendezvousAuthenticationTypeRsa2048Pem
+                                                      fullTag:@"RSA2048@"
+                                               signingHandler:nil
+                                                        error:nil];
+
+    [self common_TestVectorsWithTag:rendezvousHelper.tag];
+
+
+    rendezvousHelper
+    = [rendezvousCrypto rendezvousHelperForAuthenticationType:QredoRendezvousAuthenticationTypeRsa4096Pem
+                                                      fullTag:@"RSA4096@"
+                                               signingHandler:nil
+                                                        error:nil];
+
+    [self common_TestVectorsWithTag:rendezvousHelper.tag];
+}
+
 @end
