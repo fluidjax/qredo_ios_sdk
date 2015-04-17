6b6c273c06725ff20be9ddab72b4626d91dda6ab
Dmitry Matyukhin <dmitry@qredo.com>
2015-04-17 17:21:31 +0100

    Extract conversation crypto into separate functions and cover with unit tests

--- QredoSDK/Internal/Rendezvous/QredoConversationCrypto.m
+++ QredoSDK/Internal/Rendezvous/QredoConversationCrypto.m
@@ -9,6 +9,14 @@
 
 #import <CommonCrypto/CommonCrypto.h>
 
+#define SALT_REQUESTER_INBOUND_ENCKEY [@"iJ8LLVtLlt2tzlXz" dataUsingEncoding:NSUTF8StringEncoding]
+#define SALT_REQUESTER_INBOUND_AUTHKEY [@"7KySh0dMToM9IyzR" dataUsingEncoding:NSUTF8StringEncoding]
+#define SALT_RESPONDER_INBOUND_ENCKEY [@"BbSe15geLqnWW5Vb" dataUsingEncoding:NSUTF8StringEncoding]
+#define SALT_RESPONDER_INBOUND_AUTHKEY [@"8i6DD1mbFMv4oG9I" dataUsingEncoding:NSUTF8StringEncoding]
+#define SALT_REQUESTER_INBOUND_QUEUE [@"eeK3hieyengahp3A" dataUsingEncoding:NSUTF8StringEncoding]
+#define SALT_RESPONDER_INBOUND_QUEUE [@"Wo6ahjata4tae5ij" dataUsingEncoding:NSUTF8StringEncoding]
+#define SALT_CONVERSATION_ID [@"0HvoEAAzECt71nYp" dataUsingEncoding:NSUTF8StringEncoding]
+
 @interface QredoConversationCrypto ()
 {
     id<CryptoImpl> _crypto;
@@ -79,6 +87,67 @@
 
     
     return [QredoPrimitiveMarshallers unmarshalObject:decryptedMessageData unmarshaller:[QLFConversationMessageLF unmarshaller]];
+}
+
+
+- (NSData *)requesterInboundEncryptionKeyWithMyPrivateKey:(QredoDhPrivateKey *)myPrivateKey
+                                            yourPublicKey:(QredoDhPublicKey *)yourPublicKey
+{
+    return [_crypto getDiffieHellmanSecretWithSalt:SALT_REQUESTER_INBOUND_ENCKEY
+                                      myPrivateKey:myPrivateKey
+                                     yourPublicKey:yourPublicKey];
+
+}
+
+- (NSData *)requesterInboundAuthenticationKeyWithMyPrivateKey:(QredoDhPrivateKey *)myPrivateKey
+                                                yourPublicKey:(QredoDhPublicKey *)yourPublicKey
+{
+    return [_crypto getDiffieHellmanSecretWithSalt:SALT_REQUESTER_INBOUND_AUTHKEY
+                                      myPrivateKey:myPrivateKey
+                                     yourPublicKey:yourPublicKey];
+}
+
+
+- (NSData *)requesterInboundQueueSeedWithMyPrivateKey:(QredoDhPrivateKey *)myPrivateKey
+                                        yourPublicKey:(QredoDhPublicKey *)yourPublicKey
+{
+    return [_crypto getDiffieHellmanSecretWithSalt:SALT_REQUESTER_INBOUND_QUEUE
+                                      myPrivateKey:myPrivateKey
+                                     yourPublicKey:yourPublicKey];
+}
+
+
+- (NSData *)responderInboundEncryptionKeyWithMyPrivateKey:(QredoDhPrivateKey *)myPrivateKey
+                                            yourPublicKey:(QredoDhPublicKey *)yourPublicKey
+{
+    return [_crypto getDiffieHellmanSecretWithSalt:SALT_RESPONDER_INBOUND_ENCKEY
+                                      myPrivateKey:myPrivateKey
+                                     yourPublicKey:yourPublicKey];
+}
+
+- (NSData *)responderInboundAuthenticationKeyWithMyPrivateKey:(QredoDhPrivateKey *)myPrivateKey
+                                                yourPublicKey:(QredoDhPublicKey *)yourPublicKey
+{
+    return [_crypto getDiffieHellmanSecretWithSalt:SALT_RESPONDER_INBOUND_AUTHKEY
+                                      myPrivateKey:myPrivateKey
+                                     yourPublicKey:yourPublicKey];
+}
+
+- (NSData *)responderInboundQueueSeedWithMyPrivateKey:(QredoDhPrivateKey *)myPrivateKey
+                                        yourPublicKey:(QredoDhPublicKey *)yourPublicKey
+{
+    return [_crypto getDiffieHellmanSecretWithSalt:SALT_RESPONDER_INBOUND_QUEUE
+                                      myPrivateKey:myPrivateKey
+                                     yourPublicKey:yourPublicKey];
+}
+
+- (QredoQUID *)conversationIdWithMyPrivateKey:(QredoDhPrivateKey *)myPrivateKey
+                                yourPublicKey:(QredoDhPublicKey *)yourPublicKey
+{
+    NSData *conversationIdData = [_crypto getDiffieHellmanSecretWithSalt:SALT_CONVERSATION_ID
+                                                            myPrivateKey:myPrivateKey
+                                                           yourPublicKey:yourPublicKey];
+    return [[QredoQUID alloc] initWithQUIDData:conversationIdData];
 }
 
 @end
