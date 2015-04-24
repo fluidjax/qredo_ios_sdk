c9c55b8e58bf57e987d06285e6ef4486915a2a6e
Gabriel Radu <1342gr@gmail.com>
2015-04-24 11:25:16 +0100

    Add web socket tests for vault

---
+++ QredoSDKTests/QredoVaultWebSocketTests.m
@@ -0,0 +1,50 @@
+#import "QredoVaultTests.h"
+
+@interface QredoVaultWebSocketTests : QredoVaultTests
+
+@end
+
+@implementation QredoVaultWebSocketTests
+
+- (void)setUp {
+    self.transportType = QredoClientOptionsTransportTypeWebSockets;
+    [super setUp];
+}
+
+- (void)testPersistanceVaultId {
+    [super testPersistanceVaultId];
+}
+
+- (void)testPutItem {
+    [super testPutItem];
+}
+
+- (void)testPutItemMultiple {
+    [super testPutItemMultiple];
+}
+
+- (void)testGettingItems {
+    [super testGettingItems];
+}
+
+- (void)testEnumeration {
+    [super testEnumeration];
+}
+
+- (void)testEnumerationReturnsCreatedItem {
+    [super testEnumerationReturnsCreatedItem];
+}
+
+- (void)testEnumerationAbortsOnStop {
+    [super testEnumerationAbortsOnStop];
+}
+
+- (void)testListener {
+    [super testListener];
+}
+
+- (void)testVaultItemMetadataAndMutableMetadata {
+    [super testVaultItemMetadataAndMutableMetadata];
+}
+
+@end
