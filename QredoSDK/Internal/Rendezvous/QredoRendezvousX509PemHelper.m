5fbd674565f7e4b75e5539cefb1c031689720905
David Hearn <dhearn@users.noreply.github.com>
2015-05-11 19:39:58 +0100

    Progress on adding CRL checks via OpenSSL
    - Initial in-progress commit of X.509 authenticated rendezvous validation using
    OpenSSL.  CRL checks currently bypassed until CRLs passed into
    QredoRendezvousX509PemHelper (next bit of work).

--- QredoSDK/Internal/Rendezvous/QredoRendezvousX509PemHelper.m
+++ QredoSDK/Internal/Rendezvous/QredoRendezvousX509PemHelper.m
@@ -7,12 +7,16 @@
 #import "CryptoImpl.h"
 #import "QredoCrypto.h"
 #import "QredoCertificateUtils.h"
+#import "QredoOpenSSLCertificateUtils.h"
 #import "QredoLogging.h"
 #import "QredoAuthenticatedRendezvousTag.h"
+#import "NSData+QredoRandomData.h"
 
 @interface QredoAbstractRendezvousX509PemHelper ()
 
 @property (nonatomic) NSArray *trustedRootRefs;
+@property (nonatomic) NSArray *trustedRootPems;
+@property (nonatomic) NSArray *crlPems;
 
 @end
 
@@ -21,6 +25,7 @@
 // Salt length for RSA PSS signing (related to hash length)
 const NSInteger kX509AuthenticatedRendezvousSaltLength = 32;
 static const NSUInteger kX509AuthenticatedRendezvousEmptySignatureLength = 256;
+static const NSUInteger kRandomKeyIdentifierLength = 32;
 
 // TODO: DH - confirm the minimum length of X.509 authentication tag (i.e. single certificate with RSA 2048 bit Public key) - 2048 bit key must be at least 256 bytes long, unsure how much certificate wrapping adds
 static const NSUInteger kMinX509AuthenticationTagLength = 256;
@@ -32,7 +37,13 @@ static const NSUInteger kMinX509AuthenticationTagLength = 256;
     self = [super initWithCrypto:crypto];
     if (self) {
         
-        // TrustedRootPems is required for X.509 PEM authenticated rendezvous. Convert from PEM to SecCertificateRefs
+        // TrustedRootPems is required for X.509 PEM authenticated rendezvous
+        _trustedRootPems = [[NSArray alloc] initWithArray:trustedRootPems copyItems:YES];
+        
+        // TODO: DH - populate crlPems after adding new argument
+        _crlPems = [[NSArray alloc] init];
+
+        // Convert from PEM to SecCertificateRefs
         _trustedRootRefs = [QredoCertificateUtils getCertificateRefsFromPemCertificatesArray:trustedRootPems];
         if (!_trustedRootRefs) {
             LogError(@"Could not convert trusted root PEM certificates into SecCertificateRefs.");
@@ -55,22 +66,60 @@ static const NSUInteger kMinX509AuthenticationTagLength = 256;
     return [QLFRendezvousAuthSignature rendezvousAuthX509_PEMWithSignature:emptySignatureData];
 }
 
-- (SecKeyRef)getPublicKeyRefFromX509AuthenticationTag:(NSString *)authenticationTag error:(NSError **)error
+- (SecKeyRef)getPublicKeyRefFromX509AuthenticationTag:(NSString *)authenticationTag
+                                  publicKeyIdentifier:(NSString *)publicKeyIdentifier
+                                                error:(NSError **)error
 {
-    NSArray *certificateChainRefs = [QredoCertificateUtils getCertificateRefsFromPemCertificates:authenticationTag];
-    if (!certificateChainRefs) {
-        LogError(@"Could not get any certificate refs. Tag not valid PEM formatted X.509 cert? Authentication tag: %@", authenticationTag);
+    NSArray *pemCertificateStrings = [QredoCertificateUtils splitPemCertificateChain:authenticationTag];
+    if (!pemCertificateStrings) {
+        LogError(@"PEM cert split returned nil array.");
         updateErrorWithQredoRendezvousHelperError(error, QredoRendezvousHelperErrorAuthenticationTagInvalid, nil);
         return nil;
     }
-    
-    SecKeyRef publicKeyRef = [QredoCertificateUtils validateCertificateChain:certificateChainRefs
-                                                         rootCertificateRefs:self.trustedRootRefs];
-    if (!publicKeyRef) {
-        LogError(@"Authentication tag (certificate chain) did not validate correctly. Authentication tag: %@", authenticationTag);
+    else if (pemCertificateStrings.count <= 0) {
+        LogError(@"PEM cert split returned 0 certificates in array.");
         updateErrorWithQredoRendezvousHelperError(error, QredoRendezvousHelperErrorAuthenticationTagInvalid, nil);
+        return nil;
     }
+
+    // Subject certificate is first PEM certificate found
+    NSString *subjectPemCertificate = pemCertificateStrings[0];
     
+    // Chain (intermediate) certificates are not always present
+    NSArray *chainPemCertificates = [[NSArray alloc] init];
+
+    if (pemCertificateStrings.count > 0) {
+        // Make copy of chain certificates and then remove first entry (subject cert)
+        NSMutableArray *tempArray = [NSMutableArray arrayWithArray:pemCertificateStrings];
+        [tempArray removeObjectAtIndex:0];
+        chainPemCertificates = [tempArray copy];
+    }
+
+    LogDebug(@"Certificate chain contains %lu intermediate certificates", (unsigned long)chainPemCertificates.count);
+    
+    // TODO: DH - remove revocation check bypass!
+    
+    // Validate chain using OpenSSL, but still need a SecKeyRef to allow use of existing RSA routines (e.g. signing)
+    BOOL certificateIsValid = [QredoOpenSSLCertificateUtils validateCertificate:subjectPemCertificate
+                                                           skipRevocationChecks:YES
+                                                           chainPemCertificates:chainPemCertificates
+                                                            rootPemCertificates:self.trustedRootPems
+                                                                        pemCrls:self.crlPems
+                                                                          error:error];
+    
+    SecKeyRef publicKeyRef = nil;
+    if (certificateIsValid) {
+        publicKeyRef = [QredoOpenSSLCertificateUtils getPublicKeyRefFromPemCertificate:subjectPemCertificate
+                                                                   publicKeyIdentifier:publicKeyIdentifier
+                                                                                 error:error];
+        
+        if (!publicKeyRef) {
+            LogError(@"Could not import Public Key data from X.509 certificate. Authentication Tag: '%@'", authenticationTag);
+            updateErrorWithQredoRendezvousHelperError(error, QredoRendezvousHelperErrorAuthenticationTagInvalid, nil);
+            return nil;
+        }
+    }
+
     return publicKeyRef;
 }
 
@@ -81,6 +130,7 @@ static const NSUInteger kMinX509AuthenticationTagLength = 256;
 
 // TODO: DH - look at moving these 2 properties into the QredoAbstractRendezvousX509PemHelper as common to Create and Response helpers
 @property (nonatomic) QredoAuthenticatedRendezvousTag *authenticatedRendezvousTag;
+@property (nonatomic, copy) NSString *publicKeyIdentifier;
 @property (nonatomic) SecKeyRef publicKeyRef;
 @property (nonatomic, copy) signDataBlock signingHandler;
 
@@ -129,8 +179,13 @@ static const NSUInteger kMinX509AuthenticationTagLength = 256;
             return nil;
         }
         
+        // Generate a random ID to temporarily (for life of this object) 'name' the keys.
+        NSString *keyId = [QredoLogging hexRepresentationOfNSData:[NSData dataWithRandomBytesOfLength:kRandomKeyIdentifierLength]];
+        _publicKeyIdentifier = [keyId stringByAppendingString:@".public"];
+
         // Confirm that the authentication tag is a PEM certificate chain which validates correctly
         _publicKeyRef = [self getPublicKeyRefFromX509AuthenticationTag:_authenticatedRendezvousTag.authenticationTag
+                                                   publicKeyIdentifier:_publicKeyIdentifier
                                                                  error:error];
         if (!_publicKeyRef || (error && *error)) {
             LogError(@"X.509 authentication tag is invalid.");
@@ -140,6 +195,14 @@ static const NSUInteger kMinX509AuthenticationTagLength = 256;
         _signingHandler = signingHandler;
     }
     return self;
+}
+
+- (void)dealloc
+{
+    // Delete any keys we imported (as they should be transitory)
+    if (_publicKeyIdentifier) {
+        [QredoCrypto deleteKeyInAppleKeychainWithIdentifier:_publicKeyIdentifier];
+    }
 }
 
 - (QredoRendezvousAuthenticationType)type
@@ -194,6 +257,7 @@ static const NSUInteger kMinX509AuthenticationTagLength = 256;
 
 // TODO: DH - look at moving these 2 properties into the QredoAbstractRendezvousX509PemHelper as common to Create and Response helpers
 @property (nonatomic) QredoAuthenticatedRendezvousTag *authenticatedRendezvousTag;
+@property (nonatomic, copy) NSString *publicKeyIdentifier;
 @property (nonatomic) SecKeyRef publicKeyRef;
 
 @end
@@ -234,13 +298,27 @@ static const NSUInteger kMinX509AuthenticationTagLength = 256;
             return nil;
         }
         
-        _publicKeyRef = [self getPublicKeyRefFromX509AuthenticationTag:_authenticatedRendezvousTag.authenticationTag error:error];
+        // Generate a random ID to temporarily (for life of this object) 'name' the keys.
+        NSString *keyId = [QredoLogging hexRepresentationOfNSData:[NSData dataWithRandomBytesOfLength:kRandomKeyIdentifierLength]];
+        _publicKeyIdentifier = [keyId stringByAppendingString:@".public"];
+
+        _publicKeyRef = [self getPublicKeyRefFromX509AuthenticationTag:_authenticatedRendezvousTag.authenticationTag
+                                                   publicKeyIdentifier:_publicKeyIdentifier
+                                                                 error:error];
         if (!_publicKeyRef || (error && *error)) {
             LogError(@"Failed to validate/get public key from authentication tag.");
             return nil;
         }
     }
     return self;
+}
+
+- (void)dealloc
+{
+    // Delete any keys we imported (as they should be transitory)
+    if (_publicKeyIdentifier) {
+        [QredoCrypto deleteKeyInAppleKeychainWithIdentifier:_publicKeyIdentifier];
+    }
 }
 
 - (QredoRendezvousAuthenticationType)type
