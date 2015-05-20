ca002d069ae0c4ed67ded43706d6f038699efd8f
Dmitry Matyukhin <dmitry@qredo.com>
2015-05-20 16:32:15 +0100

    Update keychain sender tests

--- QredoSDKTests/KeychainTransporterSenderTests.swift
+++ QredoSDKTests/KeychainTransporterSenderTests.swift
@@ -68,7 +68,7 @@ class KeychainTransporterSenderTests: XCTestCase {
         var completionHandlerCalls = 0
         sender.startWithCompletionHandler { error in
             completionHandlerCalls++
-            XCTAssertNotNil(error, "There should be a cancelation error")
+            XCTAssertNil(error, "There should finish without error")
             senderCompletionExpectation?.fulfill()
             senderCompletionExpectation = nil
         }
@@ -140,31 +140,47 @@ class KeychainTransporterSenderTests: XCTestCase {
         let rendezvousTagWithURI = QredoRendezvousURIProtocol.stringByAppendingString(rendezvousTag)
         var transporterConversation : QredoConversation? = nil
 
+        var publishDeviceInfoExpectation : XCTestExpectation?
+        var receivedResponseExpectation : XCTestExpectation?
+
         let rendezvousDelegate = RendezvousBlockDelegate()
         rendezvousDelegate.responseHandler = { conversation in
             transporterConversation = conversation
 
-            transporterConversation?.publishMessage(QredoConversationMessage(value: nil, dataType: QredoKeychainTransporterMessageKeyDeviceName, summaryValues: [:]), completionHandler: { watermark, error in
-                XCTAssertNil(error, "failed to publish device info")
+            receivedResponseExpectation?.fulfill()
+
+            transporterConversation?.publishMessage(
+                QredoConversationMessage(
+                    value: nil,
+                    dataType: QredoKeychainTransporterMessageTypeDeviceInfo,
+                    summaryValues: [:]),
+                completionHandler: { watermark, error in
+                    XCTAssertNil(error, "failed to publish device info")
+
+                    publishDeviceInfoExpectation?.fulfill()
             })
         }
 
-        let rendezvousExpectation = self.expectationWithDescription("new rendezvous")
+        var receiverRendezvous : QredoRendezvous?
+        let rendezvousExpectation = self.expectationWithDescription("create rendezvous")
         receiverClient.createAnonymousRendezvousWithTag(rendezvousTag, configuration: QredoRendezvousConfiguration(conversationType: QredoKeychainTransporterConversationType, durationSeconds: 60, maxResponseCount: 1)) { rendezvous, error in
             XCTAssertNil(error, "failed to create rendezvous")
             XCTAssertNotNil(rendezvous, "rendezvous should not be nil")
 
-            rendezvous.delegate = rendezvousDelegate
-            rendezvous.startListening()
-
+            receiverRendezvous = rendezvous
             rendezvousExpectation.fulfill()
         }
         self.waitForExpectationsWithTimeout(qtu_defaultTimeout, handler: nil)
+
+        receiverRendezvous?.delegate = rendezvousDelegate
+        receiverRendezvous?.startListening()
 
         let senderMock = KeychainSenderMock()
         let sender = QredoKeychainSender(client: senderClient, delegate: senderMock)
 
         let senderCompletionExpectation = self.expectationWithDescription("sender completion")
+        receivedResponseExpectation = expectationWithDescription("received response to rendezvous")
+        publishDeviceInfoExpectation = expectationWithDescription("publish device info message")
 
         senderMock.shouldPassConfirmation = false
         senderMock.shouldDiscoverTag = rendezvousTagWithURI
@@ -177,7 +193,11 @@ class KeychainTransporterSenderTests: XCTestCase {
 
         self.waitForExpectationsWithTimeout(50, handler: nil)
 
+        receiverRendezvous?.stopListening()
+
         XCTAssertNotNil(transporterConversation, "should get response to the rendezvous")
+
+        XCTAssertTrue(senderMock.didCallDiscoverRendezvous, "Should discover rendezvous")
         XCTAssertFalse(senderMock.didCallEstablishedConnection, "Should not establish connection with the wrong conversation type")
         XCTAssertEqual(senderMock.state, KeychainSenderMock.State.Failed, "should pass verification, tag is a valid QUID")
     }
