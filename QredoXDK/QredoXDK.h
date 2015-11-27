c605dace89d3448d1b6c9cbe3d4df06fc66e2ee9
Adam Horacek <adam.horacek@me.com>
2015-11-27 11:43:36 +0000

    updated build settings

---
+++ QredoXDK/QredoXDK.h
@@ -0,0 +1,22 @@
+//
+//  QredoXDK.h
+//  QredoXDK
+//
+//  Created by Adam Horacek on 2015-11-26.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "Qredo.h"
+#import "QredoKeychain.h"
+#import "QredoTestUtils.h"
+
+//! Project version number for asda.
+FOUNDATION_EXPORT double QredoXDKVersionNumber;
+
+//! Project version string for asda.
+FOUNDATION_EXPORT const unsigned char QredoXDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QredoXDK/PublicHeader.h>
+
+
