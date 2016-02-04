e1398725f3eef48edc5022e6817941e30a4243f6
Chris Morris <chris@morris.net>
2016-02-04 16:29:28 +0000

    Fix bild numbers on CI

--- scripts/set_build_number.sh
+++ scripts/set_build_number.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-git=$(sh /etc/profile; which git)
-number_of_commits=$("$git" rev-list HEAD --count)
-git_release_version=$("$git" describe --tags --always --abbrev=0)
-
 target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
 dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 
-
-echo $target_plist
-echo $dsym_plist
+if [ -d .git ]; then
+	git=$(sh /etc/profile; which git)
+	number_of_commits=$("$git" rev-list HEAD --count)
+	git_release_version=$("$git" describe --tags --always --abbrev=0)
+	#this is a git directory
+	echo "*****************************************************"
+	echo $number_of_commits > $SRCROOT/buildnumber
+    echo $git_release_version > $SRCROOT/releasenumber
+else
+  number_of_commits=$(cat "$SRCROOT/buildnumber")
+  git_release_version=$(cat "$SRCROOT/releasenumber")
+  echo "*****************************************************"
+  echo "Commits is $number_of_commits" 
+  echo "Release is $git_release_version"
+fi;
 
 for plist in "$target_plist" "$dsym_plist"; do
   if [ -f "$plist" ]; then
@@ -17,4 +25,6 @@ for plist in "$target_plist" "$dsym_plist"; do
     /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${git_release_version#*v}" "$plist"
   fi
 done
+
+
 
