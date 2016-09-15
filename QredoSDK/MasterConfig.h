08682559483a85f90f64085136d858cf3d0cded0
Chris Morris <chris@morris.net>
2016-09-15 10:12:13 +0100

    Remove some unused tests and change credentials for testing

--- QredoSDK/MasterConfig.h
+++ QredoSDK/MasterConfig.h
@@ -5,14 +5,18 @@
 //Use this file to specify the Server
 //Note: For Tests the  AppID,AppSecret, & UserID are all specified in the file "QredoXCTestCase.m"
 
-//#define QREDO_SERVER_URL @"api.oderq.com" //dev staging
-#define QREDO_SERVER_URL @"api.qredo.com" //production
+#define QREDO_SERVER_URL @"api.oderq.com" //dev staging
+//#define QREDO_SERVER_URL @"api.qredo.com" //production
 
 
 //staging for   @"api.oderq.com"
-#define     STAGING_TEST_APPID          @"com.qredo.device.ios.test"
-#define     STAGING_TEST_APPSECRET      @"a23469be8be13768c74ca0937cec47d1"
+//#define     STAGING_TEST_APPID          @"com.qredo.device.ios.test"
+//#define     STAGING_TEST_APPSECRET      @"a23469be8be13768c74ca0937cec47d1"
+//#define     STAGING_TEST_USERID         @"testUserId"
+#define     STAGING_TEST_APPID          @"6e50b259-942d-499e-b4e2-6ceeb3d25990"
+#define     STAGING_TEST_APPSECRET      @"e2b104fdb1cb4f36b467ceabd8935b40"
 #define     STAGING_TEST_USERID         @"testUserId"
+
 
 //PRODUCTION for @"api.qredo.com"
 #define     PRODUCTION_TEST_APPID       @"com.qredo.device.ios.test"
