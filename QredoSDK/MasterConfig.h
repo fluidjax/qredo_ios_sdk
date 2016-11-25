8109852754c157e0f731734545f3ac461a23132a
Chris Morris <chris@morris.net>
2016-11-25 11:37:50 +0000

    Remove preprocessor warnings about redefinition of QREDO_SERVER_URL

--- QredoSDK/MasterConfig.h
+++ QredoSDK/MasterConfig.h
@@ -12,22 +12,22 @@
 //#define SERVER_PRODUCTION
 
 
-
-
-
 #ifdef SERVER_LOCAL
+    #undef      QREDO_SERVER_URL
     #define     QREDO_SERVER_URL    @"localhost"
     #define     SERVER_APPID        @"test"
     #define     SERVER_APPSECRET    @"cafebabe"
     #define     SERVER_USERID       @"testUserId"
 
 #elif defined SERVER_STAGING
+    #undef      QREDO_SERVER_URL
     #define     QREDO_SERVER_URL    @"api.oderq.com"
     #define     SERVER_APPID        @"6e50b259-942d-499e-b4e2-6ceeb3d25990"
     #define     SERVER_APPSECRET    @"e2b104fdb1cb4f36b467ceabd8935b40"
     #define     SERVER_USERID       @"testUserId"
 
 #elif defined SERVER_PRODUCTION
+    #undef      QREDO_SERVER_URL
     #define     QREDO_SERVER_URL    @"api.qredo.com"
     #define     SERVER_APPID        @"com.qredo.device.ios.test"
     #define     SERVER_APPSECRET    @"074af11737f877505167177726501aa0"
