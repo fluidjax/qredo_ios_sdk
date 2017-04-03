bf5d1b4d1cdad8324b3692a5a16db7ba11d2304e
Chris Morris <chris@morris.net>
2017-04-03 15:13:02 +0100

    Test Tweaks
    QredoXDK - when tested will perform all 3 sets of tests(LF, Crypto & SDK) 

--- QredoSDK/MasterConfig.h
+++ QredoSDK/MasterConfig.h
@@ -1,26 +1,25 @@
 /* HEADER GOES HERE */
 
 
-//This is what is used as the default ClientOptions - the server that a user would usually connect to
-//#define     DEFAULT_SERVER_URL      @"api.qredo.com"
-//#define     DEFAULT_USE_HTTP        NO
+/**
+    These are what is used in QredoClientOptions - the server that a user would usually connect too.
+    Usage:
+
+    QredoClientOptions *options = [QredoClientOptions alloc] initLive]  // For Developers to access the live server
+    QredoClientOptions *options = [QredoClientOptions alloc] initDev]   // For Developers to access the developement server
+    QredoClientOptions *options = [QredoClientOptions alloc] initTest]  // For Qredo Use Only - this will use the test server
+ */
 
 
 //The Live Server  [QredoClientOptions alloc] initLive]
 #define     LIVE_SERVER_URL             @"api.qredo.com"
 #define     LIVE_USE_HTTP               NO
-#define     LIVE_SERVER_APP_ID          @"test"
-#define     LIVE_SERVER_APP_SECRET      @"074af11737f877505167177726501aa0"
 
 
 
 //The Dev Server  use [QredoClientOptions alloc] initDev]
 #define     DEV_SERVER_URL              @"api-dev.oderq.com"
 #define     DEV_USE_HTTP                NO
-#define     DEV_SERVER_APP_ID           @"test"
-#define     DEV_SERVER_APP_SECRET       @"a23469be8be13768c74ca0937cec47d1"
-
-
 
 
 //The Test Server [QredoClientOptions alloc] initTest]
@@ -36,30 +35,8 @@
 #define     TEST_APP_GROUP              @"group.com.qredo.ChrisPush1"
 #define     TEST_KEYCHAIN_GROUP         @"com.qredo.ChrisPush1"
 #define     TEST_HTTP_SERVICE_URL       @"https://api-push.oderq.com:443/services"
-
-
-
-
-//Misc stuff just stored
-//#define     TEST_SERVER_URL          @"10.0.0.111"
-//#define     TEST_USE_HTTP            YES
-//#define     TEST_SERVER_APPID        @"test"
-//#define     TEST_SERVER_APPSECRET    @"cafebabe"
-//#define     TEST_SERVER_USERID       @"testUserId1"
-//#define     TEST_SERVER_USERSECRET   @"secret1"
-//#define     TEST_SERVER_USERID2      @"testUserId2"
-//#define     TEST_SERVER_USERSECRET2  @"secret2"
-//#define     TEST_APP_GROUP           @"group.com.qredo.ChrisPush1"
-//#define     TEST_KEYCHAIN_GROUP      @"com.qredo.ChrisPush1"
-//
-//
-
-
-
-
-
-
-
-
-
-
+//These APP_ID & SECRETS are not stored in the produced framework, but are used in testing to check that Live & Dev are working
+#define     LIVE_SERVER_APP_ID          @"test"
+#define     LIVE_SERVER_APP_SECRET      @"074af11737f877505167177726501aa0"
+#define     DEV_SERVER_APP_ID           @"test"
+#define     DEV_SERVER_APP_SECRET       @"a23469be8be13768c74ca0937cec47d1"
