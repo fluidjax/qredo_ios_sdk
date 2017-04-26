781e403a93ddb63edfa309b9671af0712b0dc3d8
Chris Morris <chris@morris.net>
2017-04-26 14:17:21 +0100

    Change to api-dev as test server

--- QredoSDK/MasterConfig.h
+++ QredoSDK/MasterConfig.h
@@ -26,7 +26,7 @@
 
 //The Test Server [QredoClientOptions alloc] initTest]
 //These are used in Testing to create an QredoClientOption object and pass it into the QredoClient init method
-#define     TEST_SERVER_URL             @"api-push.oderq.com"
+#define     TEST_SERVER_URL             @"api-dev.oderq.com"
 #define     TEST_USE_HTTP               NO
 #define     TEST_SERVER_APP_ID          @"test"
 #define     TEST_SERVER_APP_SECRET      @"a23469be8be13768c74ca0937cec47d1"
@@ -36,7 +36,7 @@
 #define     TEST_SERVER_USERSECRET2     @"secret2"
 #define     TEST_APP_GROUP              @"group.com.qredo.ChrisPush1"
 #define     TEST_KEYCHAIN_GROUP         @"com.qredo.ChrisPush1"
-#define     TEST_HTTP_SERVICE_URL       @"https://api-push.oderq.com:443/services"
+#define     TEST_HTTP_SERVICE_URL       @"https://api-dev.oderq.com:443/services"
 //These APP_ID & SECRETS are not stored in the produced framework, but are used in testing to check that Live & Dev are working
 #define     LIVE_SERVER_APP_ID          @"test"
 #define     LIVE_SERVER_APP_SECRET      @"074af11737f877505167177726501aa0"
