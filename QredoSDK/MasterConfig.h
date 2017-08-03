d252700a7e6d849d9eae5b2d8272dfcc7e43a014
Chris Morris <chris@morris.net>
2017-08-03 16:32:50 +0100

    Fix Broken app_secret

--- QredoSDK/MasterConfig.h
+++ QredoSDK/MasterConfig.h
@@ -16,11 +16,8 @@
 #define QREDO_DEBUG_LEVEL 1
 
 //The Live Server  [QredoClientOptions alloc] initLive]
-
 #define     LIVE_SERVER_URL             @"api.qredo.com"
-//#define     LIVE_SERVER_URL             @"api-ed.oderq.com"
 #define     LIVE_USE_HTTP               NO
-
 
 
 //The Dev Server  use [QredoClientOptions alloc] initDev]
@@ -28,17 +25,35 @@
 #define     DEV_USE_HTTP                NO
 
 
+
+
+
+
+
+
+
+
+
 //The Test Server [QredoClientOptions alloc] initTest]
 //These are used in Testing to create an QredoClientOption object and pass it into the QredoClient init method
 
-#define     TEST_SERVER_URL             @"api-dev.oderq.com"
+//Standard Test Server
+//#define     TEST_SERVER_URL             @"api-dev.oderq.com"
+//#define     TEST_USE_HTTP               NO
+//#define     TEST_SERVER_APP_ID          @"test"
+//#define     TEST_SERVER_APP_SECRET      @"a23469be8be13768c74ca0937cec47d1"
+//#define     TEST_HTTP_SERVICE_URL       @"https://api-dev.oderq.com:443/services"
+
+
+//Live as Test
+#define     TEST_SERVER_URL             @"api.qredo.com"
 #define     TEST_USE_HTTP               NO
 #define     TEST_SERVER_APP_ID          @"test"
-#define     TEST_SERVER_APP_SECRET      @"a23469be8be13768c74ca0937cec47d1"
+#define     TEST_SERVER_APP_SECRET      @"074af11737f877505167177726501aa0"
 #define     TEST_HTTP_SERVICE_URL       @"https://api-dev.oderq.com:443/services"
 
-//Use these when testing on local Qreedo-in-a-jar
-//        java -jar qredo-in-a-jar-0.19-SNAPSHOT.jar 
+
+//Use these when testing on local Qreedo-in-a-jar    use:java -jar qredo-in-a-jar-0.19-SNAPSHOT.jar
 //#define     TEST_SERVER_URL             @"127.0.0.1"
 //#define     TEST_USE_HTTP               YES
 //#define     TEST_SERVER_APP_ID          @"test"
@@ -54,6 +69,13 @@
 //#define     TEST_HTTP_SERVICE_URL       @"http://api-ed.oderq.com:8080/services"
 
 
+
+
+
+
+
+
+//Other Test Vars
 #define     TEST_SERVER_USERID          @"testUserId1"
 #define     TEST_SERVER_USERSECRET      @"secret1"
 #define     TEST_SERVER_USERID2         @"testUserId2"
